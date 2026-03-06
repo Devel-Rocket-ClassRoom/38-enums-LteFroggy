bd925c416328dbf75269781ce306472d71857228
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-06 11:38:45 +0900

    CodingTest 02 완료

--- CodingPractice-02/Program.cs
+++ CodingPractice-02/Program.cs
@@ -1,4 +1,30 @@
-using System;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+// 1. 열거형의 기본 값
+using System; {
+    Console.WriteLine($"Animal.Dragon : {(int)Animal.Dragon}, {Animal.Dragon.ToString()}");
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+// 2. Enum.GetValues
+{
+    Console.WriteLine($"Priroity 열거형의 값들 : ");
+    foreach (var v in Enum.GetValues(typeof(Priority))) {
+        Console.WriteLine($"{v.ToString()} = {(int)v}");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+enum Animal {
+    Rabbit,
+    Dragon,
+    Snake
+}
+
+enum Priority {
+    High = 1,
+    Normal = 5,
+    Low = 10
+}
